a63fead6d5a4b154fe09722b8e904388f8808771
Anirban Deb Singha <anirban12321@gmail.com>
2025-10-21 20:20:21 +0530

    chore: add context to cdk.json

--- bin/app.ts
+++ bin/app.ts
@@ -1,14 +1,14 @@
-import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
+import "source-map-support/register";
 import { RestApiStack } from "../lib/rest-api-stack";
 
 const app = new cdk.App();
 
-const envName = app.node.tryGetContext("env") ?? "dev";
+const envName = app.node.tryGetContext("env") ?? "prod";
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = process.env.CDK_DEFAULT_REGION ?? "us-east-1";
 
 new RestApiStack(app, `LambdaRestApi-${envName}`, {
-  env: { account, region },
-  stackName: `lambda-rest-api-${envName}`,
+    env: { account, region },
+    stackName: `lambda-rest-api-${envName}`,
 });
