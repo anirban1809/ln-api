98917429570e9c648b3a0d388372c15cbd979c1e
anirban <anirban12321@gmail.com>
2025-10-24 12:00:05 +0530

    feat: add localhost:8080 to list of allowed origins

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -42,7 +42,7 @@ export class RestApiStack extends Stack {
       defaultCorsPreflightOptions: {
         allowHeaders: apigw.Cors.DEFAULT_HEADERS,
         allowMethods: apigw.Cors.ALL_METHODS,
-        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowOrigins: [...apigw.Cors.ALL_ORIGINS, "http://localhost:8080"],
       },
     });
 
